26579d2291e790dac68ad2ca09e4b33d716c0110
szpetny.frank <szpetny.frank@bf6c1e66-6537-11de-9867-89bd1bae666e>
2009-06-30 16:18:02 +0000

    Now again displaying main menu. only. but there is important decision to support only-right touch mode ;p

--- trunk/CellPost/src/app/cellpost/CellPostMain.java
+++ trunk/CellPost/src/app/cellpost/CellPostMain.java
@@ -1,30 +1,48 @@
 package app.cellpost;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
-import android.widget.ArrayAdapter;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.ListView;
 
-public class CellPostMain extends ListActivity {
+public class CellPostMain extends ListActivity implements OnTouchListener{
 	private static final String TAG = "CellPostMain";
 	
 	// Menu item id
     public static final int MENU_ITEM_SELECT = Menu.FIRST;
 	
-    public final static String[] MAIN_MENU = { "Inbox",
-		   "Send e-mail",
-		   "Drafts",
-		   "Settings" };
-    
 	@Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setDefaultKeyMode(DEFAULT_KEYS_SHORTCUT);
 
-        setListAdapter(new ArrayAdapter<String>(this,
-        		android.R.layout.simple_list_item_1, MAIN_MENU));
-        getListView().setTextFilterEnabled(true);
-
+        setContentView(R.layout.main_list_look);
+                
     }
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		switch(position) {
+			case 0: System.out.println("Inbox");
+					break;
+			case 1: System.out.println("Send e-mail");
+					break;
+			case 2: System.out.println("Drafts");
+					break;
+			case 3: System.out.println("Settings");
+					break;
+		}
+		
+	}
 
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	
 }
