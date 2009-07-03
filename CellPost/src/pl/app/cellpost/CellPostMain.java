7908526aa6922adea31f015b2eff31455eabab8a
szpetny.frank <szpetny.frank@bf6c1e66-6537-11de-9867-89bd1bae666e>
2009-07-03 11:02:37 +0000

    At last there is a little of hope. Screen is changing after clicking INBOX. Nothing more works yet.

--- CellPost/src/pl/app/cellpost/CellPostMain.java
+++ CellPost/src/pl/app/cellpost/CellPostMain.java
@@ -1,9 +1,11 @@
-package app.cellpost;
+package pl.app.cellpost;
 
+import pl.app.cellpost.CellPostInternals.Emails;
 import android.app.ListActivity;
+import android.content.ContentUris;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnTouchListener;
@@ -11,23 +13,32 @@ import android.widget.ListView;
 
 public class CellPostMain extends ListActivity implements OnTouchListener{
 	private static final String TAG = "CellPostMain";
-	
-	// Menu item id
-    public static final int MENU_ITEM_SELECT = Menu.FIRST;
-	
+
 	@Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setDefaultKeyMode(DEFAULT_KEYS_SHORTCUT);
 
         setContentView(R.layout.main_list_look);
+        
+        Intent intent = getIntent();
+        if (intent.getAction() == null) {
+            intent.setAction(Intent.ACTION_PICK);
+        }
+        if (intent.getData() == null) {
+            intent.setData(Emails.CONTENT_URI);
+        }
                 
     }
 	
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
+		Uri uri = ContentUris.withAppendedId(getIntent().getData(), id);
 		switch(position) {
 			case 0: System.out.println("Inbox");
+					String action = getIntent().getAction();
+	        		if (Intent.ACTION_PICK.equals(action))
+	        			startActivity(new Intent(this, Inbox.class));
 					break;
 			case 1: System.out.println("Send e-mail");
 					break;
