10f6c60312224721901dabb4e033d1085705d619
szpetny.frank <szpetny.frank@bf6c1e66-6537-11de-9867-89bd1bae666e>
2009-07-03 12:12:49 +0000

    First working dialog :D (almost..)

--- trunk/CellPost/src/pl/app/cellpost/Inbox.java
+++ trunk/CellPost/src/pl/app/cellpost/Inbox.java
@@ -3,10 +3,15 @@
  */
 package pl.app.cellpost;
 
+import pl.app.cellpost.CellPostInternals.Accounts;
 import pl.app.cellpost.CellPostInternals.Emails;
+import android.app.AlertDialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.SimpleCursorAdapter;
@@ -23,9 +28,17 @@ public class Inbox extends ListActivity {
     public static final int MENU_ITEM_DELETE = Menu.FIRST + 1;
 
     /**
-     * The columns we are interested in from the database
+     * The columns we are interested in from the database for accounts
      */
-    private static final String[] PROJECTION = new String[] {
+    private static final String[] PROJECTION_ACCOUNTS = new String[] {
+            Accounts._ID, // 0
+            Accounts.ADDRESS, // 1
+    };
+    
+    /**
+     * The columns we are interested in from the database for emails
+     */
+    private static final String[] PROJECTION_EMAILS = new String[] {
             Emails._ID, // 0
             Emails.FROM, // 1
             Emails.SUBJECT, // 2
@@ -50,9 +63,36 @@ public class Inbox extends ListActivity {
 	        // Inform the list we provide context menus for items
 	        getListView().setOnCreateContextMenuListener(this);
 	        
+	        Cursor cursor;
+	        try {
+	        	cursor = managedQuery(Accounts.CONTENT_URI, PROJECTION_ACCOUNTS, null, null,
+	        			Accounts.DEFAULT_SORT_ORDER);
+	        } catch (SQLiteException e) {
+	        	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+	        	builder.setMessage("There is no configured e-mail accounts. " +
+	        			" Do you want to configure your e-mail now?")
+	        	       .setCancelable(false)
+	        	       .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+	        	           public void onClick(DialogInterface dialog, int id) {
+	        	                dialog.cancel();
+	        	                startActivity(new Intent(getApplicationContext(), CellPostMain.class));
+	        	           }
+	        	       })
+	        	       .setNegativeButton("No", new DialogInterface.OnClickListener() {
+	        	           public void onClick(DialogInterface dialog, int id) {
+	        	                finish();
+	        	           }
+	        	       });
+	        	AlertDialog alert = builder.create();
+	        	alert.show();
+	        	return;
+			}
+	        		
+	        
+	        
 	        // Perform a managed query. The Activity will handle closing and requerying the cursor
 	        // when needed.
-	        Cursor cursor = managedQuery(getIntent().getData(), PROJECTION, null, null,
+	        cursor = managedQuery(getIntent().getData(), PROJECTION_EMAILS, null, null,
 	                Emails.DEFAULT_SORT_ORDER);
 
 	        // Used to map notes entries from the database to views
