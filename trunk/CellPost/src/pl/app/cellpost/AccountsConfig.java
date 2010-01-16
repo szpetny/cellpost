9c81484d1c05b1afcca0cc200dff997abb85346f
szpetny.frank <szpetny.frank@bf6c1e66-6537-11de-9867-89bd1bae666e>
2010-01-16 10:31:54 +0000

    let's start again after this looong break x>

--- trunk/CellPost/src/pl/app/cellpost/AccountsConfig.java
+++ trunk/CellPost/src/pl/app/cellpost/AccountsConfig.java
@@ -9,7 +9,6 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.text.Editable;
